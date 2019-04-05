331a7f557cb162a4571b2bb5e12f1c1dc7163221
Tom Schlick <tschlick@orisintel.com>
2019-04-05 09:56:40 -0400

    add test

---
+++ tests/ModelTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace OrisIntel\AuditLog\Tests;
+
+
+class ModelTest extends TestCase
+{
+    /** @test */
+    public function testing()
+    {
+        $this->assertTrue(true);
+    }
+}
